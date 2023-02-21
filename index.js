0cc023d3a68187141505e75cf398d0e12fa6e615
AP696 <addyphilips66@gmail.com>
2023-02-21 15:53:33 +0000

    2

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const {Band} = require('./Band')
 const {Musician} = require('./Musician')
+const {Song} = require('./Song')
 
 Musician.belongsTo(Band);
-Band.hasMany(Musician)
+Band.hasMany(Musician);
+
+Band.belongsToMany(Song, { through: "BandsSongs" });
+Song.belongsToMany(Band, { through: "BandsSongs" });
 
 module.exports = {
     Band,
-    Musician
+    Musician,
+    Song
 };
